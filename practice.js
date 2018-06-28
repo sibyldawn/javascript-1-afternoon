b4a803193f34ef336494a6033fb87d9740c7344e
Sibyl Dawn Pomeroy <Sibyl@Sibyls-MacBook-Air.local>
2018-06-27 21:29:08 -0700

    Complete Problems

--- practice.js
+++ practice.js
@@ -90,7 +90,7 @@ let ruff = dog.bark();
 function looper(arr){
   var mySum = 0;
   for(var i=0; i<= arr.length; i++){
-    if(arr[i]% 2 === 0 || arr[i]>= 100){
+    if(arr[i]% 2 !== 0 || arr[i]>= 100){
       arr[i] += mySum;
     }
   }
